a439576b2ce0dd09dd551ead6da13047c6858349
ThaiBinh <thaibinh719@gmail.com>
2017-05-29 22:18:59 +0700

    commit

--- SERVER.js
+++ SERVER.js
@@ -7,17 +7,51 @@ var server  = require('http').Server(app);
 var io = require('socket.io')(server);
 server.listen(3000);
 
-// io.on('connection',function(socket){
-//     console.log('co nguoi ket noi: '+socket.id);
+arrUsers=[];
 
-//     socket.on('disconnect',function(){
-//         console.log(socket.id + 'Ngat ket noi');
-//     })
-//      socket.on('client-send-data',function(data){
-//         console.log(data);
-//         io.sockets.emit('server-send-data',data+'888')
-//     })
-// });
+io.on('connection',function(socket){
+    console.log('co nguoi ket noi: '+ socket.id);
+
+    // socket.on('disconnect',function(){
+    //     console.log(socket.id + 'Ngat ket noi');
+    // })
+     socket.on('client-send-username',function(data){
+         if(arrUsers.indexOf(data)>=0){
+             socket.emit('server-send-dki-thatbai');
+         }else{
+             arrUsers.push(data);
+             socket.Username=data;
+             socket.emit('server-send-dki-thanhcong',data);
+             io.sockets.emit('server-send-danhsach-user', arrUsers)
+         }
+        // io.sockets.emit('server-send-data',data)
+    });
+
+    socket.on('logout',function(){
+        arrUsers.splice(
+            arrUsers.indexOf(socket.Username),1
+        )
+
+        socket.broadcast.emit('server-send-danhsach-user',arrUsers);
+        
+    });
+
+    socket.on('client-send-message',function(data){
+        io.sockets.emit('server-send-message',{username:socket.Username,nd:data})
+        
+    });
+
+    socket.on('toi-dang-go-chu',function(data){
+        var message = socket.Username + 'dang go chu'
+        io.sockets.emit('ai-do-dang-go-chu',message);
+        
+    });
+    socket.on('toi-dung-go-chu',function(data){
+        io.sockets.emit('ai-do-dung-go-chu');
+        
+    });
+
+});
 
 app.get('/',function (req, res) {
     res.render('trangchu')
--- public/process.js
+++ public/process.js
@@ -1,13 +1,57 @@
-// var socket = io('http://localhost:3000');
+var socket = io('http://localhost:3000');
 
-        // socket.on("server-send-data",function(data){
-        //     $("#noidung").append(data+', ')
-        // });
+        socket.on("server-send-dki-thatbai",function(){
+           alert('User name bi trung (Dang ky khong thanh cong)')
+        });
+        socket.on("server-send-dki-thanhcong",function(data){
+            $('#currentUser').html(data)
+            $("#loginform").hide(2000);
+            $("#chatForm").show(1000);
+        });
+        socket.on("server-send-danhsach-user",function(data){
+            $('#boxContent').html('');
+           data.forEach(function(element) {
+                $('#boxContent').append("<div class='useronline'>"+element+"</div>")
+           }, this);
+        });
+        socket.on("server-send-message",function(data){
+            $('#listMessage').append("<div class='ms'>"+data.username+ ": "+ data.nd+ "</div>")
+        });
+        socket.on("ai-do-dang-go-chu",function(data){
+            $('#thongbao').html(data)
+        });
+
+         socket.on("ai-do-dung-go-chu",function(data){
+            $('#thongbao').html()
+        });
 
         $(document).ready(function(){
             // $("#mrA").on('click',function(){
             //     socket.emit("client-send-data","hello");
             // })
-            $("#loginForm").show();
-            $("#chatFrom").hide();
+            $("#loginform").show();
+            $("#chatForm").hide();
+
+            $('#txtMessage').focusin(function(){
+                socket.emit('toi-dang-go-chu');
+            })
+
+            $('#txtMessage').focusout(function(){
+                socket.emit('toi-dung-go-chu');
+            })
+
+            $("#btnRegister").on('click',function(){
+                socket.emit('client-send-username',$("#txtUserName").val())
+            })
+
+            $("#btnLogout").on('click',function(){
+                socket.emit('logout');
+                $("#loginform").show(2000);
+                $("#chatForm").hide(1000);
+            })
+
+            $("#btnSendMessage").on('click',function(){
+                socket.emit('client-send-message',$("#txtMessage").val())
+            })
+
         });
